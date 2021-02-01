ffe71c857b02d760987b4f2a034fabab09215ece
AnakonDRaG <anakondrag@gmail.com>
2021-02-01 20:01:21 +0300

    save script

---
+++ ScriptsHelperLib/CameraShake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using Scripts.Helper;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Helper
+{
+    public class CameraShake : SingletonBehaviour<CameraShake>
+    {
+        [SerializeField]private float duration = 0.15f;
+        [SerializeField]private float magnitude = 0.05f;
+        
+
+        public IEnumerator ShakeCamera()
+        {
+            Vector3 originalPosition = transform.localPosition;
+            float elapsed = 0.0f;
+
+            while(elapsed < duration)
+            {
+                float x = Random.Range(-1f, 1f) * magnitude;
+                float y = Random.Range(-1f, 1f) * magnitude;
+
+                transform.localPosition = new Vector3(originalPosition.x + x, originalPosition.y + y, originalPosition.z);
+                elapsed += Time.deltaTime;
+
+                yield return null;
+            }
+
+            transform.localPosition = originalPosition;
+        }
+    }
+}
---
+++ ScriptsHelperLib/SingletonBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+namespace Scripts.Helper
+{
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T _instance;
+
+        public static bool IsAwake => _instance != null;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = (T) FindObjectOfType(typeof(T));
+                    if (_instance == null)
+                    {
+                        var goName = typeof(T).ToString();
+
+                        var go = GameObject.Find(goName);
+                        if (go == null)
+                        {
+                            go = new GameObject();
+                            go.name = goName;
+                        }
+
+                        _instance = go.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        public virtual void OnApplicationQuit()
+        {
+            _instance = null;
+        }
+
+        protected void SetParent(string parentGOName)
+        {
+            if (parentGOName != null)
+            {
+                var parentGO = GameObject.Find(parentGOName);
+                if (parentGO == null)
+                {
+                    parentGO = new GameObject();
+                    parentGO.name = parentGOName;
+                }
+
+                transform.parent = parentGO.transform;
+            }
+        }
+    }
+}
